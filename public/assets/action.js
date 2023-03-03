de12cfd7610327aec7de46ca9f9242f52300f0a1
Vicky-Feng <71288189+Vicky-Feng@users.noreply.github.com>
2023-03-03 15:11:26 -0500

    html page

---
+++ public/assets/action.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+
+    $('form').on('submit', function(){
+
+        // var item = $('form input');
+        // console.log(item.serializeArray());
+
+        $.ajax({
+            type: 'POST',
+            url: '/questionnaire',
+            data: $(this).serializeArray(),
+            success: function(data){
+                // do something with the data via front-end framework
+                // Make the submit button red, disabled and saying Thank you
+                $("#bb").css("background-color", "red");
+                $("#bb").prop("disabled", "true");
+                $("#bb").text("Thank you!");
+            }
+        });
+        return false;
+    });
+});
